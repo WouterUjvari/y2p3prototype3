06ce3f16751e6304fc4ab74190e2e2177f93d4f7
Lajos de Graaf <lajosdegraaf@hotmail.com>
2018-02-12 21:56:09 +0100

    Package Added

---
+++ Introduction-To-HTC-Vive-Final/Introduction To HTC Vive - Final/Assets/GunScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunScript : MonoBehaviour {
+
+    public Animator anim;
+    public AudioSource gunshotSound;
+    public float triggerAxis;
+
+    //public bool canBeFired;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))  //canBeFired )
+        {
+            FireGun();
+        }
+
+        anim.SetFloat("fTriggerAxis", Input.GetAxis("Horizontal"));
+
+        if (Input.GetAxis("Horizontal") >= 0.9f)
+        {
+            FireGun();
+            //triggerAxis = 0;
+
+        }
+
+    }
+
+    public void FireGun()
+    {
+
+        //canBeFired = false;
+
+        anim.SetTrigger("pShoot");
+
+        print("Shoot!");
+
+
+
+    }
+
+    public void PlaySound()
+    {
+        gunshotSound.Play();
+        anim.SetBool("bShoot", false);
+    }
+
+    public void switchState()
+    {
+        //canBeFired = true;
+    }
+
+        
+
+
+}
